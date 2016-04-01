6427b73b811e38d58d0a9a1e7e49e1f6f4b4ec0e
anurag saran <anurag.saran@gmail.com>
2016-04-01 16:42:00 +0000

    Update dbtest.php

--- dbtest.php
+++ dbtest.php
@@ -1,9 +1,9 @@
 <?php
 $dbhost = getenv("MYSQL_SERVICE_HOST");
 $dbport = getenv("MYSQL_SERVICE_PORT");
-$dbuser = getenv("DATABASE_USER");
-$dbname = getenv("DATABASE_NAME");
-$dbpwd = getenv("DATABASE_PASSWORD");
+$dbuser = getenv("MYSQL_USER");
+$dbname = getenv("MYSQL_DATABASE");
+$dbpwd = getenv("MYSQL_PASSWORD");
  
 $connection = mysqli_connect($dbhost.":".$dbport, $dbuser, $dbpwd, $dbname) or die("Error " . mysqli_error($connection));
 $query = "SELECT * from users" or die("Error in the consult.." . mysqli_error($connection));
